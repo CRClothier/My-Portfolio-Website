f61e046e1b9a5b345fbeae8f9e9b1bfc3555e579
MacCrazyman <leonardopareja.fl@hotmail.com>
2021-11-08 12:21:16 -0500

    Eslint fix

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
 const hamburger = document.querySelector('.hamburger');
-let mobileMenu = document.querySelector('#mobile_menu')
-const crossButton = document.querySelector('#cross')
+const mobileMenu = document.querySelector('#mobile_menu');
+const crossButton = document.querySelector('#cross');
 
 function showMenu() {
-  mobileMenu.classList.replace('hidden','show_menu')
+  mobileMenu.classList.replace('hidden', 'show_menu');
 }
 function hideMenu() {
-  mobileMenu.classList.replace('show_menu','hidden')
+  mobileMenu.classList.replace('show_menu', 'hidden');
 }
 
-
-hamburger.addEventListener('click', showMenu)
-crossButton.addEventListener('click',hideMenu)
+hamburger.addEventListener('click', showMenu);
+crossButton.addEventListener('click', hideMenu);
