5659938f9ec4f6f1f3a992afd8a2e09b81ef104c
MacCrazyman <leonardopareja.fl@hotmail.com>
2021-11-08 12:13:35 -0500

    Configure menu functionality

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 const hamburger = document.querySelector('.hamburger');
 let mobileMenu = document.querySelector('#mobile_menu')
+const crossButton = document.querySelector('#cross')
 
 function showMenu() {
   mobileMenu.classList.replace('hidden','show_menu')
 }
+function hideMenu() {
+  mobileMenu.classList.replace('show_menu','hidden')
+}
 
 
-hamburger.addEventListener('click', showMenu);
+hamburger.addEventListener('click', showMenu)
+crossButton.addEventListener('click',hideMenu)
